6c7346579730c6501643a3490e21057edae7840c
Carlos Arturo Cuellar <carturoch@gmail.com>
2016-09-16 11:02:22 -0400

    Add speed units.

---
+++ test/units/speed_test.exs
@@ -0,0 +1,14 @@
+defmodule Units.SpeedTest do
+  use ExUnit.Case
+
+  import ExUc, only: [convert: 2]
+
+  test "Miles per hour to knots" do
+    conversion = convert("20 km/h", "mps")
+    |> convert("mph")
+    |> convert("knots")
+
+    assert conversion == "10.98 knots"
+  end
+
+end
